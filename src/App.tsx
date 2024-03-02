2fbe6cd1abb920864a4ba6cd78311aa6814dd49f
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-03-02 18:51:02 -0300

    empty old vite structure, project is blank now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    null
-  )
+  return null;
 }
 
-export default App
+export default App;
