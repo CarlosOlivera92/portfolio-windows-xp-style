f44e0f72ba3284b2c5361caf22e014f267ce078e
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-03-03 00:25:09 -0300

    add styles and create start button, added images for background and implement button in taskbar

---
+++ src/components/molecules/Start-button/StartButton.tsx
@@ -0,0 +1,10 @@
+import styles from "./StartButton.module.css";
+
+const StartButton = () => {
+  return (
+    <div className={styles.startButton}>
+      <p>Start</p>
+    </div>
+  );
+};
+export default StartButton;
--- src/components/organisms/Taskbar/Taskbar.tsx
+++ src/components/organisms/Taskbar/Taskbar.tsx
@@ -1,6 +1,11 @@
+import StartButton from "../../molecules/Start-button/StartButton";
 import styles from "./Taskbar.module.css";
 
 const Taskbar = () => {
-  return <div className={styles.taskbar}></div>;
+  return (
+    <div className={styles.taskbar}>
+      <StartButton />
+    </div>
+  );
 };
 export default Taskbar;
